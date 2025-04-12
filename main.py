7ada5049d4eb494c8b0784899ed42b1b5c7e1da4
Константин <konstantin@MacBook-Air.local>
2025-04-12 14:35:18 +0700

    add menu without calc sys > 9

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def decimal_to_other_system(num):
+def decimal_to_other_system():
+    num = int(input("Напечатай число, которое хочешь перевести: \n"))
     num_sys = int(input("Выбери систему счисления в которую хочешь перевести число: "))
     result = ""
 
@@ -8,9 +9,13 @@ def decimal_to_other_system(num):
     return result[::-1]
 
 # функция реализована работает, но стоило бы подумать как ее оптимизировать
-def other_system_to_decimal(num_sys, num):
-    num_sys = num_sys.split()
-    num = num[::-1]
+# не продумал про то, что в СС где основание > 9 вводятся буквы английского алфавита
+def other_system_to_decimal():
+    num_sys = input("выбери из какой системы в какую хочешь перевести"
+                    "(сначала в какой сс стоит потом в какую перевести)"
+                    "через пробел, пример: 10 2\n").split()
+    num = input("выбери число которое хочешь перевести: \n")[::-1]
+    sys_alphabet = 'ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$&'
     index_num = []
     result = 0
 
@@ -20,8 +25,21 @@ def other_system_to_decimal(num_sys, num):
         result += index_num[i][0] * (int(num_sys[0])**index_num[i][1])
     return result
 
+def main():
+    while True:
+        choice = int(input('1 перевод из 10 в другую СС\n2 перевод из любой в 10 СС\n'
+                       '3 перевод из любой в любую СС\nТВОЙ ВЫБОР(1 2 3):\n'))
+        if choice == 1:
+            print(f"РЕЗУЛЬТАТ: {decimal_to_other_system()}")
+        elif choice == 2:
+            print(f"РЕЗУЛЬАТ: {other_system_to_decimal()}")
+        else:
+            print("Неверно введена кнопка, попробуй еще раз\nПОДСКАЗКА - 1 2 3")
+        print("\n\n")
+        # print(other_system_to_decimal(input("выбери из какой системы в какую хочешь перевести"
+        #                                     "(сначала в какой сс стоит потом в какую перевести)"
+        #                                     "через пробел, пример: 10 2\n"),
+        #                               input("выбери число которое хочешь перевести: \n")))
 
-print(other_system_to_decimal(input("выбери из какой системы в какую хочешь перевести"
-                                    "(сначала в какой сс стоит потом в какую перевести)"
-                                    "через пробел, пример: 10 2\n"),
-                              input("выбери число которое хочешь перевести: \n")))
+
+main()
