0f38f5fdf51b4fb331779eb45717b30532a1a3d4
Константин <konstantin@MacBook-Air.local>
2025-04-12 15:11:26 +0700

    add calc sys > 9

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def decimal_to_other_system():
         num //= num_sys
     return result[::-1]
 
+
 # функция реализована работает, но стоило бы подумать как ее оптимизировать
 # не продумал про то, что в СС где основание > 9 вводятся буквы английского алфавита
 def other_system_to_decimal():
@@ -16,14 +17,19 @@ def other_system_to_decimal():
                     "через пробел, пример: 10 2\n").split()
     num = input("выбери число которое хочешь перевести: \n")[::-1]
     sys_alphabet = 'ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$&'
+    sys_alphabet_nums = [i for i in range(10, 66)]
     index_num = []
     result = 0
 
     for i in range(len(num)):
-        index_num.append([int(num[i]), i])
+        if num[i] in sys_alphabet:
+            index_num.append([sys_alphabet_nums[sys_alphabet.index(num[i])], i])
+        else:
+            index_num.append([int(num[i]), i])
     for i in range(len(index_num)):
         result += index_num[i][0] * (int(num_sys[0])**index_num[i][1])
     return result
+
 
 def main():
     while True:
@@ -36,10 +42,6 @@ def main():
         else:
             print("Неверно введена кнопка, попробуй еще раз\nПОДСКАЗКА - 1 2 3")
         print("\n\n")
-        # print(other_system_to_decimal(input("выбери из какой системы в какую хочешь перевести"
-        #                                     "(сначала в какой сс стоит потом в какую перевести)"
-        #                                     "через пробел, пример: 10 2\n"),
-        #                               input("выбери число которое хочешь перевести: \n")))
 
 
 main()
