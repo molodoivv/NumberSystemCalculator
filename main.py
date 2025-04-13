52b6ea66ebe8e04b8fc2853db87421c09fd27adb
Константин <konstantin@MacBook-Air.local>
2025-04-13 16:21:39 +0700

    add correct out for sys > 9 (letters)

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-def decimal_to_other_system():
-    num = int(input("Напечатай число, которое хочешь перевести: \n"))
+def decimal_to_other_system(num):
     num_sys = int(input("Выбери систему счисления в которую хочешь перевести число: "))
+    sys_alphabet = "++++++++++ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$&"
     result = ""
 
     while num != 0:
-        result += str(int(num) % num_sys)
+        if int(num) % num_sys > 9:
+            result += sys_alphabet[int(num) % num_sys]
+        else:
+            result += str(int(num) % num_sys)
         num //= num_sys
     return result[::-1]
 
 
-# функция реализована работает, но стоило бы подумать как ее оптимизировать
-# не продумал про то, что в СС где основание > 9 вводятся буквы английского алфавита
+# функция реализована и работает, но стоило бы подумать как ее оптимизировать
 def other_system_to_decimal():
-    num_sys = input("выбери из какой системы в какую хочешь перевести"
-                    "(сначала в какой сс стоит потом в какую перевести)"
-                    "через пробел, пример: 10 2\n").split()
+    num_sys = int(input("напечатай из какой СС хочешь перевести число: \n"))
     num = input("выбери число которое хочешь перевести: \n")[::-1]
-    sys_alphabet = 'ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$&'
+    sys_alphabet = "ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$&"
     sys_alphabet_nums = [i for i in range(10, 66)]
     index_num = []
     result = 0
@@ -27,18 +27,24 @@ def other_system_to_decimal():
         else:
             index_num.append([int(num[i]), i])
     for i in range(len(index_num)):
-        result += index_num[i][0] * (int(num_sys[0])**index_num[i][1])
+        result += index_num[i][0] * (num_sys**index_num[i][1])
     return result
+
+
+def other_to_other_system():
+    return decimal_to_other_system(other_system_to_decimal())
 
 
 def main():
     while True:
-        choice = int(input('1 перевод из 10 в другую СС\n2 перевод из любой в 10 СС\n'
-                       '3 перевод из любой в любую СС\nТВОЙ ВЫБОР(1 2 3):\n'))
+        choice = int(input("1 перевод из 10 в другую СС\n2 перевод из любой в 10 СС\n"
+                       "3 перевод из любой в любую СС\nТВОЙ ВЫБОР(1 2 3):\n"))
         if choice == 1:
-            print(f"РЕЗУЛЬТАТ: {decimal_to_other_system()}")
+            print(f"РЕЗУЛЬТАТ: {decimal_to_other_system(int(input("Напечатай число, которое хочешь перевести: \n")))}")
         elif choice == 2:
             print(f"РЕЗУЛЬАТ: {other_system_to_decimal()}")
+        elif choice == 3:
+            print(f"РЕЗУЛЬАТ: {other_to_other_system()}")
         else:
             print("Неверно введена кнопка, попробуй еще раз\nПОДСКАЗКА - 1 2 3")
         print("\n\n")
