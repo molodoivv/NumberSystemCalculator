c61b7b1693b216fa621263643a93b2c9c6cb8aa8
Константин <konstantin@MacBook-Air.local>
2025-04-13 16:38:30 +0700

    add mini optimization

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ def decimal_to_other_system(num):
 def other_system_to_decimal():
     num_sys = int(input("напечатай из какой СС хочешь перевести число: \n"))
     num = input("выбери число которое хочешь перевести: \n")[::-1]
-    sys_alphabet = "ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$&"
-    sys_alphabet_nums = [i for i in range(10, 66)]
+    sys_alphabet = "++++++++++ABCDEFGHIJKLMNOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$&"
     index_num = []
     result = 0
 
     for i in range(len(num)):
         if num[i] in sys_alphabet:
-            index_num.append([sys_alphabet_nums[sys_alphabet.index(num[i])], i])
+            index_num.append([sys_alphabet.index(num[i]), i])
         else:
             index_num.append([int(num[i]), i])
     for i in range(len(index_num)):
@@ -35,6 +34,7 @@ def other_to_other_system():
     return decimal_to_other_system(other_system_to_decimal())
 
 
+# заменить if else на реализацию через словарь ? {кнопка : функция}
 def main():
     while True:
         choice = int(input("1 перевод из 10 в другую СС\n2 перевод из любой в 10 СС\n"
